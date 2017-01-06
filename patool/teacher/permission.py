6c7deac3fa541ccaf4bd9468930f7ef1e229a0fd
Léon McGregor <lonmcgregor@outlook.com>
2017-01-06 12:24:31 +0000

    Teacher site

--- patool/teacher/permission.py
+++ patool/teacher/permission.py
@@ -5,9 +5,12 @@ from django.contrib.auth.models import User, Group
 def is_teacher(f):
     teacher_group = Group.objects.get(name="teacher")
 
-    def is_teacher_internal(request, **kwargs):
+    def is_teacher_internal(request, **args):
         if teacher_group in request.user.groups.all():
-            return f(request, kwargs)
+            if not args:
+                return f(request)
+            else:
+                return f(request, args)
         else:
             return HttpResponseForbidden()
 
--- patool/teacher/views.py
+++ patool/teacher/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.conf import settings
 import teacher.permission as p
 import student.models as m
 import teacher.forms as f
+from django.db import transaction
 
 
 @login_required()
 @p.is_teacher
-def index(request, kwargs):
+def index(request):
     me = request.user
     enrolled_in = m.EnrolledUser.objects.filter(login=me)
     my_courses = []
@@ -22,19 +22,92 @@ def index(request, kwargs):
 
 @login_required()
 @p.is_teacher
-def create_course(request, kwargs):
-    pass
+def create_course(request):
+    if request.method == "POST":
+        return create_course_update(request, request.POST)
+    return create_course_render(request)
+
+
+def create_course_render(request):
+    detail = {
+        "course_name": "New Course",
+        "courseworks": [],
+        "uf": f.CourseForm({"student": str(request.user)+','})
+    }
+    return render(request, 'teacher/edit_course.html', detail)
+
+
+@transaction.atomic
+def create_course_update(request, new_details):
+    owner = request.user
+
+    # todo ADD VALIDATION - dont want overwriting IDs etc.
+    updated_form = f.CourseForm(new_details)
+    if not updated_form.is_valid():
+        raise Exception("validity problem")
+    new_id = updated_form.cleaned_data['id']
+    new_name = updated_form.cleaned_data['name']
+    new_students = updated_form.cleaned_data['student'].strip().strip(',').split(',')
+    new_students_list = list(map(lambda w: w.strip(), new_students))
+
+    course = m.Course(id=new_id, name=new_name)
+    course.save()
+
+    for item in new_students_list:
+        current_user = m.User.objects.get(username=item)
+        new_item = m.EnrolledUser(login=current_user, course=course)
+        new_item.save()
+
+    if str(owner) not in new_students_list:
+        new_item = m.EnrolledUser(login=owner, course=course)
+        new_item.save()
+
+    return redirect('edit_course', c=new_id)
 
 
 @login_required()
 @p.is_teacher
 def edit_course(request, kwargs):
-    # todo check exists
+    requested_course_id = kwargs['c']
+    if request.method == "POST":
+        edit_course_update(request, requested_course_id, request.POST)
+    return edit_course_render(request, requested_course_id)
+
+
+@transaction.atomic
+def edit_course_update(request, course_id, new_details):
     # todo ADD VALIDATION - dont want overwriting IDs etc.
-    if request.method=="POST":
-        #todo save updates
-        pass
-    course = m.Course.objects.get(id=kwargs['c']) #TODO fix this
+    owner = request.user
+
+    old_course = m.Course.objects.get(id=course_id)
+    updated_form = f.CourseForm(new_details)
+    if not updated_form.is_valid():
+        raise Exception("validity problem")
+    new_id = updated_form.cleaned_data['id']
+    new_name = updated_form.cleaned_data['name']
+    new_students = updated_form.cleaned_data['student'].strip().strip(',').split(',')
+    new_students_list = list(map(lambda w: w.strip(), new_students))
+
+    enrollments = m.EnrolledUser.objects.filter(course=old_course)
+    for item in enrollments:
+        if item.login == owner:
+            continue
+        if str(item.login) not in new_students_list:
+            item.delete()
+    for item in new_students_list:
+        current_user = m.User.objects.get(username=item)
+        exists = m.EnrolledUser.objects.filter(login=current_user).filter(course=old_course)
+        if exists.count() != 1:
+            new_item = m.EnrolledUser(login=current_user, course=old_course)
+            new_item.save()
+
+    old_course.id = new_id
+    old_course.name = new_name
+    old_course.save()
+
+
+def edit_course_render(request, requested_course_id):
+    course = m.Course.objects.get(id=requested_course_id)
     enrollments = m.EnrolledUser.objects.filter(course=course)
     students = ""
     for eu in enrollments:
@@ -44,6 +117,7 @@ def edit_course(request, kwargs):
     update_form = f.CourseForm(initial)
     detail = {
         "course_name": course,
+        "course_id": requested_course_id,
         "courseworks": courseworks,
         "uf": update_form
     }
@@ -52,20 +126,64 @@ def edit_course(request, kwargs):
 
 @login_required()
 @p.is_teacher
-def create_coursework(request):
-    pass
+def create_coursework(request, kwargs):
+    requested_course_id = kwargs['c']
+    if request.method == "POST":
+        return create_coursework_update(request.POST, requested_course_id)
+    return create_coursework_render(request)
+
+
+def create_coursework_render(request):
+    detail = {
+        "courseworks": {"name": "New Coursework"},
+        "cw_form": f.CourseworkForm(),
+        "solutions": [],
+        "tests": []
+    }
+    return render(request, 'teacher/edit_cw.html', detail)
+
+
+@transaction.atomic
+def create_coursework_update(new_details, course_id):
+    # todo ADD VALIDATION - dont want overwriting IDs etc.
+    updated_form = f.CourseworkForm(new_details)
+    if not updated_form.is_valid():
+        raise Exception("validity problem")
+    new_name = updated_form.cleaned_data['name']
+    new_descriptor = updated_form.cleaned_data['descriptor']
+    new_state = updated_form.cleaned_data['state']
+
+    course = m.Course.objects.get(id=course_id)
+    coursework = m.Coursework(name=new_name, descriptor=new_descriptor, course=course,
+                              state=new_state)
+    coursework.save()
+    return redirect('edit_cw', c=coursework.id)
 
 
 @login_required()
 @p.is_teacher
-def edit_coursework(request, c=None):
-    #has solutions
-    #has test cases
-    if request.method=="POST":
-        pass
-    coursework = m.Coursework.objects.get(id=c['c'])
+def edit_coursework(request, kwargs=None):
+    coursework = m.Coursework.objects.get(id=kwargs['c'])
+    if request.method == "POST":
+        edit_coursework_update(request.POST, coursework)
+    return edit_coursework_render(request, coursework)
+
+
+def edit_coursework_update(new_details, old_coursework):
+    updated_form = f.CourseworkForm(new_details)
+    if not updated_form.is_valid():
+        raise Exception("validity problem")
+    old_coursework.name = updated_form.cleaned_data['name']
+    old_coursework.descriptor = updated_form.cleaned_data['descriptor']
+    old_coursework.state = updated_form.cleaned_data['state']
+    old_coursework.save()
+
+
+def edit_coursework_render(request, coursework):
     solutions = m.Solution.objects.filter(coursework=coursework)
     tests = []
+    # has solutions
+    # has test cases
     for solution in solutions:
         if solution.test is not None:
             tests.append(solution.test)
