0859e492b81e1bfd2cfb300b28fd19170b159804
LonMcGregor <lemcgregor3@outlook.com>
2017-07-06 14:21:44 +0100

    Additions to make things easier to get started

--- patool/patool/settings.py
+++ patool/patool/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = "/home/cs4/lm356/peer-testing"
+BASE_DIR = "/home/lonm/dissertationproject/patool"
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = '$cn(%o3r-mdc8ku_$0xnyo4bf+g!gxt4%0!i^hi%3)odr_4%!w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["www2.macs.hw.ac.uk", "anubis.macs.hw.ac.uk", "137.195.15.99"]
+ALLOWED_HOSTS = ["www2.macs.hw.ac.uk", "anubis.macs.hw.ac.uk", "137.195.15.99", "0.0.0.0", "127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -136,7 +136,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/peer-testing/static_files/'
+HTTP_PREFIX = ''
+# if this is beign served as a sub-site of another, specify
+# the prefix with leading slash and NO trailing slash
+STATIC_URL = HTTP_PREFIX + '/static_files/'
 # this is the url that is used in the templates
 #  this is absolute so should have prepended paths
 STATIC_URL_REDIRECT = 'static_files/'
@@ -146,18 +149,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'var/static/')
 # this is the place on the disk where the static files are looked for
 # everything after the static url is appened to this path
 
-MEDIA_URL = '/peer-testing/'
+MEDIA_URL = HTTP_PREFIX + '/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'var/uploads')
 MEDIA_TMP_TEST = os.path.join(BASE_DIR, 'var/tmp/test')
 
 
 # Auth URLconf
-LOGIN_URL = '/peer-testing/student/login'
-LOGOUT_REDIRECT_URL = '/peer-testing/student/'
-
-# at most 20 1mb files per upload
-MAX_FILES_PER_SUBMISSION = 20
-MAX_FILESIZE_FOR_UPLOADS = 1024 * 1024 * 1
+LOGIN_URL = HTTP_PREFIX + '/student/login'
+LOGOUT_REDIRECT_URL = HTTP_PREFIX + '/student/'
 
 # dont run dangerous actions as apache
 owner = os.stat(BASE_DIR)
